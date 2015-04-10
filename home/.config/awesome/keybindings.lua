c1d8f4812a2b46bbbe350de6f74b1173eac9af3c
Gareth Griffiths <gareth@synbyte.com>
2015-04-10 19:24:11 +0200

    name update

--- home/.config/awesome/keybindings.lua
+++ home/.config/awesome/keybindings.lua
@@ -53,7 +53,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    awful.key({ modkey },            "r",     function () promptbox[mouse.screen]:run() end),
 
     awful.key({ modkey }, "x",
       function ()

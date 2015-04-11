ffb85de60ac6459d12cd52651ae6d72d2cf85307
Gareth Griffiths <gareth@synbyte.com>
2015-04-11 16:55:35 +0200

    luakit is looking much better now

--- home/.config/luakit/theme.lua
+++ home/.config/luakit/theme.lua
@@ -1,69 +1,70 @@
---------------------------
--- Default luakit theme --
---------------------------
-
 local theme = {}
 
--- Default settings
+theme.red			= "#CC6666"
+theme.yellow		= "#F0C674"
+theme.green			= "#B5BD68"
+theme.cyan			= "#8ABEB7"
+theme.blue			= "#81A2BE"
+theme.magenta		= "#B294BB"
+theme.dark_white	= "#969896"
+theme.white			= "#EEEEEE"
+theme.divider		= "#333333"
+theme.black			= "#222222"
+
 theme.font = "monospace normal 9"
-theme.fg   = "#fff"
-theme.bg   = "#000"
+theme.fg   = theme.dark_white
+theme.bg   = theme.black
 
--- Genaral colours
-theme.success_fg = "#0f0"
-theme.loaded_fg  = "#33AADD"
-theme.error_fg = "#FFF"
-theme.error_bg = "#F00"
+theme.success_fg = theme.green
+theme.loaded_fg  = theme.black
+theme.error_fg = theme.red
+theme.error_bg = theme.black
 
--- Warning colours
-theme.warning_fg = "#F00"
-theme.warning_bg = "#FFF"
+theme.warning_fg = theme.yellow
+theme.warning_bg = theme.black
 
--- Notification colours
-theme.notif_fg = "#444"
-theme.notif_bg = "#FFF"
+theme.notif_fg = theme.cyan
+theme.notif_bg = theme.black
 
--- Menu colours
-theme.menu_fg                   = "#000"
-theme.menu_bg                   = "#fff"
-theme.menu_selected_fg          = "#000"
-theme.menu_selected_bg          = "#FF0"
-theme.menu_title_bg             = "#fff"
-theme.menu_primary_title_fg     = "#f00"
-theme.menu_secondary_title_fg   = "#666"
+theme.menu_fg                   = theme.black
+theme.menu_bg                   = theme.dark_white
+theme.menu_selected_fg          = theme.red
+theme.menu_selected_bg          = theme.black
+theme.menu_title_bg             = theme.black
+theme.menu_primary_title_fg     = theme.red
+theme.menu_secondary_title_fg   = theme.dark_white
 
 -- Proxy manager
-theme.proxy_active_menu_fg      = '#000'
-theme.proxy_active_menu_bg      = '#FFF'
-theme.proxy_inactive_menu_fg    = '#888'
-theme.proxy_inactive_menu_bg    = '#FFF'
+theme.proxy_active_menu_fg      = theme.white
+theme.proxy_active_menu_bg      = theme.black
+theme.proxy_inactive_menu_fg    = theme.dark_white
+theme.proxy_inactive_menu_bg    = theme.black
 
 -- Statusbar specific
-theme.sbar_fg         = "#fff"
-theme.sbar_bg         = "#000"
+theme.sbar_fg         = theme.dark_white
+theme.sbar_bg         = theme.black
 
 -- Downloadbar specific
-theme.dbar_fg         = "#fff"
-theme.dbar_bg         = "#000"
-theme.dbar_error_fg   = "#F00"
+theme.dbar_fg         = theme.dark_white
+theme.dbar_bg         = theme.black
+theme.dbar_error_fg   = theme.red
 
 -- Input bar specific
-theme.ibar_fg           = "#000"
-theme.ibar_bg           = "#fff"
+theme.ibar_fg           = theme.dark_white
+theme.ibar_bg           = theme.black
 
 -- Tab label
-theme.tab_fg            = "#888"
-theme.tab_bg            = "#222"
-theme.tab_ntheme        = "#ddd"
-theme.selected_fg       = "#fff"
-theme.selected_bg       = "#000"
-theme.selected_ntheme   = "#ddd"
-theme.loading_fg        = "#33AADD"
-theme.loading_bg        = "#000"
+theme.tab_fg            = theme.dark_white
+theme.tab_bg            = theme.black
+theme.tab_ntheme        = theme.red
+theme.selected_fg       = theme.green
+theme.selected_bg       = theme.black
+theme.selected_ntheme   = theme.cyan
+theme.loading_fg        = theme.yellow
+theme.loading_bg        = theme.black
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg          = "#0F0"
-theme.notrust_fg        = "#F00"
+theme.trust_fg          = theme.green
+theme.notrust_fg        = theme.red
 
 return theme
--- vim: et:sw=4:ts=8:sts=4:tw=80

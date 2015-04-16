3a17f17eb6435fbb438e172524ccd028c3483df5
Gareth Griffiths <gareth@synbyte.com>
2015-04-16 18:45:25 +0200

    Better looking critical error messages

--- home/.config/awesome/themes/default/theme.lua
+++ home/.config/awesome/themes/default/theme.lua
@@ -17,12 +17,12 @@ theme.black			= "#222222"
 
 theme.bg_normal     = theme.black
 theme.bg_focus      = theme.black
-theme.bg_urgent     = theme.red
+theme.bg_urgent     = theme.black
 theme.bg_minimize   = theme.black
 
 theme.fg_normal     = theme.dark_white
 theme.fg_focus      = theme.white
-theme.fg_urgent     = theme.white
+theme.fg_urgent     = theme.red
 theme.fg_minimize   = theme.dark_white
 
 theme.border_width  = "1"
@@ -44,13 +44,12 @@ naughty.config.defaults.bg               = theme.bg_focus
 naughty.config.defaults.border_color     = theme.divider
 naughty.config.defaults.border_width     = 1
 naughty.config.defaults.hover_timeout    = nil
-naughty.config.presets.critical.timeout          = 5
 naughty.config.defaults.ontop            = true
 naughty.config.defaults.font             = theme.font
 naughty.config.defaults.icon             = nil
 naughty.config.defaults.icon_size        = 16
 naughty.config.presets.critical.fg		 = theme.fg_urgent
-naughty.config.presets.critical.bg		 = theme.bg_urgent
+naughty.config.presets.critical.bg		 = theme.black
 
 theme.icon_theme = nil
 theme.taglist_squares_sel   	= theme.img_dir .. "/taglist/taglist_squarefw.png"

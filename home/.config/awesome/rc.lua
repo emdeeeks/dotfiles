1ada5cb9904b97d9107477442994bccca563b9e9
Gareth Griffiths <gareth@synbyte.com>
2015-05-29 11:09:00 +0200

    finally switching to tyrannical for tagging

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -9,6 +9,7 @@ wibox = require("wibox")
 beautiful = require("beautiful")
 naughty = require("naughty")
 menubar = require("menubar")
+tyrannical = require("tyrannical")
 
 modkey = "Mod4"
 home   = os.getenv("HOME")
--- home/.config/awesome/tags.lua
+++ home/.config/awesome/tags.lua
@@ -1,30 +1,85 @@
-tags = {
-	names = {
-		"1:web",
-		"2:mail",
-		"3:skype",
-		"4:git",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9"
-	},
-	layout = {
-		layouts[3],
-		layouts[3],
-		layouts[3],
-		layouts[3],
-		layouts[3],
-		layouts[3],
-		layouts[3],
-		layouts[3],
-		layouts[3]
+tyrannical.tags = {
+    {
+        name        = "1:web",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+        instance    = {"web"},
+        class       = {
+            "luakit", "chromium"
+        }
+    },
+    {
+        name        = "2:mail",
+        init        = true,
+        exclusive   = true,
+        --screen    = screen.count()>1 and 2 or 1,-- Setup on screen 2 if there is more than 1 screen, else on screen 1
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+        class = {
+            "evolution"
+        }
+    },
+    {
+        name        = "3:skype",
+        init        = true,
+        exclusive   = true,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+        class  = {
+            "skype"
+        }
+    },
+    {
+        name        = "4:git",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+    },
+    {
+        name        = "5",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+    },
+    {
+        name        = "6",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+    },
+    {
+        name        = "7",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+    },
+    {
+        name        = "8",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
+    },
+    {
+        name        = "9",
+        init        = true,
+        exclusive   = false,
+        screen      = {1,2},
+        layout      = awful.layout.suit.tile,
     },
 }
 
-for s = 1, screen.count() do
-  tags[s] = awful.tag(tags.names, s, tags.layout)
-end
+-- Ignore the tag "exclusive" property for the following clients (matched by classes)
+tyrannical.properties.intrusive = {
+    "Xephyr",
+}
 
-
+tyrannical.properties.floating = {
+    "flie-roller", "skype"
+}

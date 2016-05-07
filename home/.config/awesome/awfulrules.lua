2eed5122f6f7544bf0edd13fa60ea4112bc67c1e
Gareth Griffiths <gareth@synbyte.com>
2016-05-07 09:05:08 +0200

    updating rules

--- home/.config/awesome/awfulrules.lua
+++ home/.config/awesome/awfulrules.lua
@@ -1,30 +1,63 @@
 awful.rules.rules = {
-	{
-		rule = { },
-		properties = {
-			border_width = beautiful.border_width,
-			border_color = beautiful.border_normal,
-			focus = true,
-			keys = clientkeys,
-			buttons = clientbuttons,
-            size_hints_honor = false	
-	    }
-	},
-	{
-		rule = { class = "gimp" },
-		properties = { floating = true }
-	},
-	{
-		rule = { class = "luakit" },
---		tag = tags[screen.count()][1]
-	},
-	{
-		rule = { class = "evolution" },
---		tag = tags[screen.count()][2]
-	},
-	{
-		rule = { class = "skype" },
---		tag = tags[screen.count()][3],
-		properties = { floating = true }
-	},
+    {
+        rule = { },
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = true,
+            keys = clientkeys,
+            buttons = clientbuttons,
+            size_hints_honor = false
+        }
+    },
+    {
+        rule = { class = "qutebrowser"  },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "work")
+        end
+    },
+    {
+        rule = { class = "URxvt", name = "gtypist" },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "misc")
+        end
+    },
+    {
+        rule = { class = "URxvt", name = "mutt"  },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "mail")
+        end
+    },
+    {
+        rule = { class = "URxvt", name = "vit"  },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "misc")
+        end
+    },
+    {
+        rule = { class = "URxvt", name = "newsbeuter"  },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "misc")
+        end
+    },
+    {
+        rule = { class = "URxvt", name = "lnav"  },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "sys")
+        end
+    },
+    {
+        rule = { class = "URxvt", name = "glances"  },
+        callback = function(c)
+            awful.client.property.set(c, "overwrite_class", "sys")
+        end
+    },
+    {
+        rule = { class = "gimp" },
+        properties = { floating = true }
+    },
+    {
+        rule = { class = "skype" },
+        properties = { floating = true }
+    },
 }

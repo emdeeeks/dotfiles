a2d2b7aa5866689761b57a06f75a4d0500824415
Gareth Griffiths <gareth@synbyte.com>
2017-12-02 22:37:19 +0000

    deleting a bunch of stuff

--- home/.config/awesome/layouts.lua
+++
@@ -1,8 +0,0 @@
-layouts = {
-    awful.layout.suit.tile,
-    awful.layout.suit.max,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-}

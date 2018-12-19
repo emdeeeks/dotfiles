9ac216bd457d1d3bbe8a537c5734b3b888e7b390
Gareth Griffiths <gareth+github@synbyte.com>
2018-12-19 21:26:56 +0000

    Windows will always need a border now if I'm going mouseless

--- home/.config/awesome/conf.d/signals.lua
+++ home/.config/awesome/conf.d/signals.lua
@@ -2,12 +2,8 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 
 client.connect_signal("focus", function(c)
-    if c.maximized then -- no borders if only 1 client visible
-        c.border_width = 0
-    elseif #awful.screen.focused().clients > 1 then
-        c.border_width = beautiful.border_width
-        c.border_color = beautiful.border_focus
-    end
+    c.border_width = beautiful.border_width
+    c.border_color = beautiful.border_focus
 end)
 
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)

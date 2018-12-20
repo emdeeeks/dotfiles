df1e0ea7db2f3dc2ee6d594b8437efe04fc02c3c
Gareth Griffiths <gareth+github@synbyte.com>
2018-12-20 22:35:56 +0000

    This is far better, + removing redshift. Doesn't seem to be working well with my graphics card

--- home/.config/awesome/conf.d/autorun.lua
+++ home/.config/awesome/conf.d/autorun.lua
@@ -1,8 +1,24 @@
+local awful = require("awful")
+
 local apps = {
-    "redshift",
     "dropbox",
-    "xrandr-setup"
+    "xrandr-setup",
+    "setxkbmap -config ~/.keyboard",
+    "unclutter -root"
 }
+
+function run_once(prg, times)
+    if not prg then
+        do return nil end
+    end
+    times = times or 1
+    count_prog = tonumber(io.popen('ps aux | grep "' .. string.gsub(prg, ":", " ") .. '" | grep -v grep | wc -l')) or 0
+    if times > count_prog then
+        for l = count_prog, times-1 do
+            awful.spawn.with_shell(prg)
+        end
+    end
+end
 
 for k,app in pairs(apps) do
     run_once(app)

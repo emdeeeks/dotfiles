673f7ddcf04a6040549293061c5a99a1bc5f4586
Gareth Griffiths <gareth+github@synbyte.com>
2019-01-06 18:47:05 +0000

    Clearing this up

--- home/.config/awesome/conf.d/signals.lua
+++ home/.config/awesome/conf.d/signals.lua
@@ -2,8 +2,9 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 
 client.connect_signal("focus", function(c)
-    c.border_width = beautiful.border_width
     c.border_color = beautiful.border_focus
 end)
 
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("unfocus", function(c)
+    c.border_color = beautiful.border_normal
+end)

977e0551b5122b964b555557ba4590e67e252c7a
Gareth Griffiths <gareth+github@synbyte.com>
2019-01-06 18:52:26 +0000

    Formatting + Tags now load from config.yml

--- home/.config/awesome/conf.d/tags.lua
+++ home/.config/awesome/conf.d/tags.lua
@@ -1,11 +1,8 @@
 local awful = require("awful")
+local tags = require("config").get("tags")
 
 awful.screen.connect_for_each_screen(function(s)
-    local tags = {}
-    for i = 1, 9 do
-        table.insert(tags, i)
-    end
     awful.tag(
-        tags
-    , s, awful.layout.layouts[1])
+        tags, s, awful.layout.layouts[1]
+    )
 end)

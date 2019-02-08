ad1db139c72338237e5dd7ae9d698badcfc44a73
Gareth Griffiths <gareth+github@synbyte.com>
2019-02-08 00:47:10 +0000

    Dynamic keykinding. Placement stay the same no matter if colemak or qwerty is being used

--- home/.config/awesome/main_keys.lua
+++ home/.config/awesome/main_keys.lua
@@ -1,20 +1,51 @@
 local keys = {}
 
-keys.home_row = {
-    { 'a','s','d','f' },
-    { 'h','j','k','l' }
+keys.rows = {
+    { 'q','w','e','r','t','y','u','i','o','p' },
+    { 'a','s','d','f','g','h','j','k','l',';' },
+    { 'z','x','c','v','b','n','m',',','.','/' }
 }
 
 if io.popen("setxkbmap -query|grep 'variant'|awk '{print $2}'"):read() == 'colemak' then
-    keys.home_row = {
-        { 'a','r','s','t' },
-        { 'n','e','i','o' }
+    keys.rows = {
+        { 'q','w','f','p','g','j','l','u','y',';' },
+        { 'a','r','s','t','d','h','n','e','i','o' },
+        { 'z','x','c','v','b','k','m',',','.','/' }
     }
 end
 
-keys.KEY_LEFT = keys.home_row[2][1]
-keys.KEY_DOWN = keys.home_row[2][2]
-keys.KEY_UP = keys.home_row[2][3]
-keys.KEY_RIGHT = keys.home_row[2][4]
+keys.focus_prev_client = keys.rows[1][3]
+keys.focus_next_client = keys.rows[1][4]
+keys.swap_with_prev_client = keys.rows[2][3]
+keys.swap_with_next_client = keys.rows[2][4]
+keys.move_client_to_prev_screen = keys.rows[3][3]
+keys.move_client_to_next_screen = keys.rows[3][4]
+
+keys.focus_prev_screen = keys.rows[3][7]
+keys.focus_next_screen = keys.rows[3][8]
+keys.decrease_width = keys.rows[2][5]
+keys.increase_width = keys.rows[2][6]
+keys.rofi_run = keys.rows[1][5]
+keys.rofi_window = keys.rows[1][6]
+keys.help = keys.rows[3][9]
+keys.open_terminal = keys.rows[3][10]
+
+keys.restart_awesome = keys.rows[1][1]
+
+keys.next_layout = 'space'
+
+keys.above_tags = {
+    keys.rows[1][7],
+    keys.rows[1][8],
+    keys.rows[1][9],
+    keys.rows[1][10]
+}
+
+keys.tags = {
+    keys.rows[2][7],
+    keys.rows[2][8],
+    keys.rows[2][9],
+    keys.rows[2][10]
+}
 
 return keys

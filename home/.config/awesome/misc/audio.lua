4e38dfc6c90c1fb2ae4df3cf44ffa21b0f81d3ea
Gareth Griffiths <gareth+github@synbyte.com>
2019-10-28 00:48:05 +0000

    This is much more basic

--- home/.config/awesome/misc/audio.lua
+++ home/.config/awesome/misc/audio.lua
@@ -1,9 +1,3 @@
-local audio = {}
-
-local naughty = require("naughty")
-local awful = require("awful")
-local wibox = require("wibox")
-local beautiful = require("beautiful")
 local awful = require("awful")
 
 local function parse_amixer_output(output)
@@ -12,28 +6,11 @@ local function parse_amixer_output(output)
     return is_muted, tonumber(volume)
 end
 
+local audio = {}
+
 function audio.set_volume(percentage)
     local volume_change = tostring(math.abs(percentage)) .. '%' .. (percentage < 0 and '-' or '+')
-    awful.spawn.easy_async('amixer set Master ' .. volume_change, function(stdout)
-        local is_muted, volume = parse_amixer_output(stdout)
-
-        local progress = wibox.widget {
-            color = beautiful.green,
-            background_color = beautiful.fg_normal,
-            forced_height = 10,
-            value = volume,
-            max_value = 100,
-            widget = wibox.widget.progressbar,
-            type = 'notification'
-        }
-
-        nid = naughty.notify({
-            text = "ok",
-            replaces_id = nid,
-            timeout = 30 
-        }).id
-        awful.placement.centered(nid)
-    end)
+    awful.spawn.easy_async('amixer -D pulse sset Master ' .. volume_change, function() end)
 end
 
 function audio.toggle_mute()

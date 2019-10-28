6e97327844fc4e191e889450c4444859c55f7a78
Gareth Griffiths <gareth+github@synbyte.com>
2019-10-28 00:37:06 +0000

    Setting something more generic

--- home/.config/qutebrowser/conf.d/content.py
+++ home/.config/qutebrowser/conf.d/content.py
@@ -1,1 +1,1 @@
-c.content.headers.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"

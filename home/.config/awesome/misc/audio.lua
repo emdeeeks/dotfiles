612ed468ed2d3e4afc5fb5c8399edb4a1920e96d
Gareth Griffiths <gareth+git@synbyte.com>
2020-02-23 22:36:12 +0000

    Audio controls were werking perfectly, then stopped. This fixes it. No idea why it stopped working

--- home/.config/awesome/misc/audio.lua
+++ home/.config/awesome/misc/audio.lua
@@ -10,11 +10,11 @@ local audio = {}
 
 function audio.set_volume(percentage)
     local volume_change = tostring(math.abs(percentage)) .. '%' .. (percentage < 0 and '-' or '+')
-    awful.spawn.easy_async('amixer -D pulse sset Master ' .. volume_change, function() end)
+    awful.spawn.easy_async('amixer -D pulse set Master ' .. volume_change, function() end)
 end
 
 function audio.toggle_mute()
-    awful.spawn("amixer -D pulse sset Master toggle")
+    awful.spawn("amixer -D pulse set Master toggle")
 end
 
 return audio

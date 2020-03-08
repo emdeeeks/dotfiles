09a1616221469ea9669a005429a51227729c9b26
Gareth Griffiths <gareth+git@synbyte.com>
2020-03-08 14:26:48 +0000

    Add new clients as slaves

--- home/.config/awesome/conf.d/signals.lua
+++ home/.config/awesome/conf.d/signals.lua
@@ -1,6 +1,16 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 
+client.connect_signal("manage", function (c, startup)
+    if not startup then
+        awful.client.setslave(c)
+        if not c.size_hints.user_position and not c.size_hints.program_position then
+            awful.placement.no_overlap(c)
+            awful.placement.no_offscreen(c)
+        end
+    end
+end)
+
 client.connect_signal("focus", function(c)
     c.border_color = beautiful.border_focus
 end)
